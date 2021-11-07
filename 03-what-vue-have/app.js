f89938711c6faf99ccc8e9d9f5e7d69be5e2f7b1
Hollarka <kofranek@gmail.com>
2021-11-07 23:34:34 +0100

    modul3-4 cykly

--- 03-what-vue-have/app.js
+++ 03-what-vue-have/app.js
@@ -8,6 +8,12 @@ Vue.createApp({
     */
    data: ()=>({
        myHtml: '<h1>Vue 3 app</h1>',
-        title: 'Hlavička'
+        title: 'Hlavička',
+        person: {
+            firstName: 'Jiří',
+            lastName: 'Kofránek',
+            age: 71
+        },
+        items: [1,2,3,4,5,6]
    })
 }).mount('#app')
