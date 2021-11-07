abd8ab67a4177b04fcb710fc172817ef943d714a
Hollarka <kofranek@gmail.com>
2021-11-07 04:17:36 +0100

    modul2-16

--- 02-vue-basic/app.js
+++ 02-vue-basic/app.js
@@ -9,10 +9,12 @@ const App = {
   },
   methods: {
     inputChangeHandler (event) {
+      /*
       console.log(
         'volán: inputChangeHandler event.target.value=',
         event.target.value
       )
+      */
       this.inputValue = event.target.value
     },
     addNewNote () {
@@ -20,17 +22,17 @@ const App = {
         this.notes.push(this.inputValue)
         this.inputValue = ''
       }
-      console.log('this.notes=', this.notes)
+      //console.log('this.notes=', this.notes)
     },
     removeNote (index) {
-      console.log('removeNote index=', index)
+      //console.log('removeNote index=', index)
       this.notes.splice(index, 1)
     },
     naVelkaPismena (item) {
       return item.toUpperCase()
     },
     numberOfChars () {
-      console.log('volá se numberChars()')
+      //console.log('volá se numberChars()')
       let nChar = 0
       this.notes.forEach(item => {
         nChar += item.length
@@ -40,12 +42,27 @@ const App = {
   },
   computed: {
     numberOfCharsComputed () {
-      console.log('volá se numberCharsComputed()')
+      //console.log('volá se numberCharsComputed()')
       let nChar = 0
       this.notes.forEach(item => {
         nChar += item.length
       })
       return nChar
+    }
+  },
+  watch: {
+    // in Vue 3: When watching an array, the callback will only trigger when the array is replaced.
+    // If you need to trigger on mutation, the deep option must be specified.
+    /*
+    notes (value) { //nefunguje ve Vue 3
+      console.log('notes value changed =', value) 
+    },
+    */
+    inputValue (value) {
+      console.log('inputValue changed', value)
+      if (value.length > 10) {
+        this.inputValue = 0
+      }
     }
   }
 }
