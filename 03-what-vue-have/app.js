1f2d78f6c2993692f495d77915fac1d27648805b
Hollarka <kofranek@gmail.com>
2021-11-08 00:10:41 +0100

    modul3-7 filtrace seznamu

--- 03-what-vue-have/app.js
+++ 03-what-vue-have/app.js
@@ -1,24 +1,29 @@
 Vue.createApp({
-    /*
+  /*
     data(){
       return  {
           
       }
     }
     */
-   data: ()=>({
-       myHtml: '<h1>Vue 3 app</h1>',
-        title: 'Hlavička',
-        person: {
-            firstName: 'Jiří',
-            lastName: 'Kofránek',
-            age: 71
-        },
-        items: [1,2,3,4,5,6]
-   }),
-   methods: {
-     stopPropagation(event){
+  data: () => ({
+    myHtml: '<h1>Vue 3 app</h1>',
+    title: 'Hlavička',
+    person: {
+      firstName: 'Jiří',
+      lastName: 'Kofránek',
+      age: 71
+    },
+    items: [1, 2, 3, 4, 5, 6]
+  }),
+  methods: {
+    stopPropagation (event) {
       event.stopPropagation()
-     }
-   }
+    }
+  },
+  computed: {
+    evenItems () {
+      return this.items.filter(i => i % 2 === 0)
+    }
+  }
 }).mount('#app')
