991ce3acf4bfd7e629c45a6c36a0b592aad63bb1
Hollarka <kofranek@gmail.com>
2021-11-08 01:58:13 +0100

    modul4-2 virtual DOM a render

--- 04-how-vue-works/app.js
+++ 04-how-vue-works/app.js
@@ -1,15 +1,34 @@
-const app=Vue.createApp({
-   data() {
-       return {
-           title: 'to je z template'
-       }
-   },
-   template:  `
-    <div class="card center">
-        <h1>{{title}}</h1>
-        <button class ="btn" @click="title='Vue 3'">Změnit hlavičku</button>
-    </div>
-   `
+const h = Vue.h
+const app = Vue.createApp({
+  data () {
+    return {
+      title: 'to je z template'
+    }
+  },
+  methods: {
+    changeTitle () {
+      this.title = 'Vue 3'
+    }
+  },
+  //    template:  `
+  //     <div class="card center">
+  //         <h1>{{title}}</h1>
+  //         <button class ="btn" @click="changeTitle">Změnit hlavičku</button>
+  //     </div>
+  //    `,
+  render () {
+      return (
+          h('div', 
+          {class: 'card center'}, 
+          [
+              h('h1',{},this.title),
+              h('button',{class: 'btn',
+                         onClick: this.changeTitle}, 'Změnit hlavičku')
+          ]
+          )
+        
+        )
+  }
 })
 
 app.mount('#app')
