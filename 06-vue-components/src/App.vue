b6a30cc7a87b76f6f7aea5c5cd8eafb4ba0606c1
Hollarka <kofranek@gmail.com>
2021-11-09 01:53:56 +0100

    modul6-03

--- 06-vue-components/src/App.vue
+++ 06-vue-components/src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>
-    <h1>This is title</h1>
-    <the-header></the-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Aktuální novinky {{ now }}</h2>
+    </div>
+
+    <div class="card" v-for="item in news" :key="item">
+      <h3>{{ item }}</h3>
+      <button class="btn" @click="isOpen = !isOpen">Otevřít</button>
+      <p v-if="isOpen">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        numquam, est culpa labore delectus placeat ipsum consequuntur
+        accusantium.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  data () {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: ['první novinka', 'druhá novinka'],
+      isOpen: false
+    }
+  }
 }
 </script>
 
--- 06-vue-components/src/TheHeader.vue
+++ 06-vue-components/src/TheHeader.vue
@@ -1,7 +1,10 @@
 <template>
-  <h2>{{title}}</h2>
+  <h2>{{ title }}</h2>
   <hr />
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sequi aliquid officia sed. Harum, voluptas ipsam doloribus voluptates nam debitis.</p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati sequi
+    aliquid officia sed. Harum, voluptas ipsam doloribus voluptates nam debitis.
+  </p>
 </template>
 
 <script>
